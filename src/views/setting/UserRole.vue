d0513e81d475ad6af013cb2db0e5a9dd348d8cb4
Xiaoqing Tan <txqing@gmail.com>
2022-10-12 19:43:55 +0800

    add userrole page form

--- src/views/setting/UserRole.vue
+++ src/views/setting/UserRole.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+const config = {
+  url: "",
+  title: "Form 表单",
+  fields: [
+    {
+      prop: "username",
+      title: "用户名",
+      placeholder: "UserName",
+      rules: [{ required: true }],
+    },
+    {
+      prop: "password",
+      title: "密码",
+      placeholder: "PassWord",
+      rules: [{ required: true }],
+    },
+  ],
+};
+</script>
 <template>
-  <h1>Role</h1>
+  <vd-form :config="config"></vd-form>
 </template>
